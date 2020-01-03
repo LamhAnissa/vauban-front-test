003726b1b7bcdebedeae9086a829ff2f5479d768
Anissa Lamh <anissa.lamhaouar@gmail.com>
2020-01-03 10:52:32 +0100

    Add Header and Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+     <v-footer
+      color="secondary"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-col
+          class="secondary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>VAUBAN Front End Test</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app-bar
+      app
+      color="secondary"
+      dark
+    >
+      <div class="d-flex align-center">
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="../assets/Star_warsyellow.png"
+          width="110"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+</template>
