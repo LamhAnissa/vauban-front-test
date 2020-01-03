e6993e20cb386e053efaa28a3ca81178b4e17c08
Anissa Lamh <anissa.lamhaouar@gmail.com>
2020-01-03 14:43:26 +0100

    Add test for search function

---
+++ cypress/integration/examples/search.spec.js
@@ -0,0 +1,45 @@
+/// <reference types="Cypress" />
+import {characters} from '../../fixtures/characters';
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  
+
+  it('should filter and return right number of cards', () => {
+    let cardcount=0;
+   cy.get('input[placeholder="Search"]').type('Luke').then(()=>{
+       cy.get('.v-card').each(()=>cardcount++).then(()=>
+           expect(cardcount).to.equal(1)
+       )
+   })
+   
+  })
+
+  it('should filter and return right cards', () => {
+    let cardcount=0;
+   cy.get('input[placeholder="Search"]').type('Luke').then(()=>{
+       cy.get('.v-card').then((card)=>{
+        let title= card.children('.v-card__title')[0].textContent;
+        expect(title).to.contain("Luke")
+       }
+       )
+   })
+   
+  })
+
+  it('should filter and return no card', () => {
+    let btncount=0;
+
+   cy.get('input[placeholder="Search"]').type('00000').then(()=>{
+    cy.get('button[disabled="disabled"]').each(()=>btncount++).then(()=>
+        expect(btncount).to.equal(1)
+    )})
+   })
+   })
+   
+
+
+  
