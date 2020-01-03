6f2364bdf74a125e3e9abbfe8a176abc0e27aa0e
Anissa Lamh <anissa.lamhaouar@gmail.com>
2020-01-03 14:21:18 +0100

    Add Cypress dependencies + first displaying test

---
+++ cypress/integration/examples/display.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="Cypress" />
+import {characters} from '../../fixtures/characters';
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  
+
+  it('should display cards corresponding to the number of characters', () => {
+   let charactersnumber= characters.length;
+   let cardcount=0;
+
+   cy.get('.v-card').each(()=>{cardcount++;}).then(()=>assert.isTrue(cardcount==charactersnumber, 'vals equal'));
+   
+  })
+
+  it('should contains character info', () => {
+    let firstChar= characters[0];
+    cy.get('.v-card').then((card)=>{ 
+    let title= card.children('.v-card__title')[0].textContent;
+    let subtitle= card.children('.v-card__subtitle')[0].textContent;
+    let text= card.children('.v-card__text')[0].textContent;
+    expect(title).to.equal(firstChar.name);
+    expect(subtitle).to.contain("Caracteristics");
+    expect(text).to.contain(firstChar.height);
+    expect(text).to.contain(firstChar.mass);
+  })
+
+  })
+
+  it('should display search bar', () => {
+   expect('input[placeholder="Search"]').to.exist;
+  })
+
+  
+})
