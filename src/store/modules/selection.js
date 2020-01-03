11d954746e16538c94639dfccb2d09f8102c4eca
Anissa Lamh <anissa.lamhaouar@gmail.com>
2020-01-03 11:46:38 +0100

    Start working on vuex side with store creation

---
+++ src/store/modules/selection.js
@@ -0,0 +1,24 @@
+
+const state = {
+    mycharacters: []
+  }
+
+  const getters = {
+    mySelection: state => state.mycharacters
+  }
+  
+
+  const actions = {
+  }
+  
+
+  const mutations = {
+
+  }
+  
+  export default {
+    state,
+    getters,
+    actions,
+    mutations
+  }
